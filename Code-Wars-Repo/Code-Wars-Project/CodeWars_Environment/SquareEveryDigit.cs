c29baba5b6443637681fe7a25631880e91c78420
jeff Cate <Jeff.a.cate@gmail.com>
2020-05-08 12:42:00 -0500

    init

---
+++ Code-Wars-Repo/Code-Wars-Project/CodeWars_Environment/SquareEveryDigit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars_Environment
+{
+    class SquareEveryDigit
+    {
+
+        public static void Run()
+        {
+            // get number to test - input
+            int testNumber = GetNumber();
+            //square number
+            int squaredNumber = SquareDigits(testNumber);
+
+            Console.WriteLine($"{testNumber} processed is {squaredNumber}");
+            Console.ReadLine();
+        }
+        public static int SquareDigits(int testNumber)
+        {
+            testNumber
+            // parse number to int[]
+            foreach(char number in testNumber)
+            {
+                
+            }
+            // square each number and store in a string
+
+            // return result[]
+            return 0;
+        }
+
+        private static int GetNumber()
+        {
+            return 9119;
+        }
+    }
+}
