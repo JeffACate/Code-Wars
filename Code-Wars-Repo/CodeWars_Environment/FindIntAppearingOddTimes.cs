1c6990f75d1bf8352295535bef2f790806cdc477
jeff Cate <Jeff.a.cate@gmail.com>
2020-07-01 13:54:40 -0500

    Kata completed: Find the odd int.

--- Code-Wars-Repo/CodeWars_Environment/FindIntAppearingOddTimes.cs
+++ Code-Wars-Repo/CodeWars_Environment/FindIntAppearingOddTimes.cs
@@ -13,11 +13,20 @@ namespace CodeWars_Environment
             int[] numbers = new int[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 };
             int number = FindNumberAppearingOddTimes(numbers);
             Console.WriteLine(number);
+            Console.ReadLine();
         }
         private static int FindNumberAppearingOddTimes(int[] numbers)
         {
-            int number = 0;
-            return number;
+            foreach(int num in numbers)
+            {
+                int count = 0;
+                count = numbers.Count(c => c == num);
+                if (count % 2 != 0)
+                {
+                    return num;
+                }
+            }
+            return numbers[0];
         }
     }
 }
