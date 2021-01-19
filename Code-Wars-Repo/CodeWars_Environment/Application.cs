0f46bdf6905708efce2f932ae6e4cc3fcfdd9070
Jeff.A.Cate <Jeff.A.Cate@gmail.com>
2021-01-19 15:07:14 -0600

    Setup new kata

--- Code-Wars-Repo/CodeWars_Environment/Application.cs
+++ Code-Wars-Repo/CodeWars_Environment/Application.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CodeWars_Environment;
 
 namespace CodeWars_Environment
 {
@@ -15,7 +16,8 @@ namespace CodeWars_Environment
             //DuplicateEncoder.Run();
             //FindIntAppearingOddTimes.Run();
             //TribonacciSequence.Run();
-            ListFiltering.Run();
+            /* ListFiltering.Run(); 1408: Incomplete */
+            SplitStringIntoPair.Run();
         }
     }
 }
---
+++ Code-Wars-Repo/CodeWars_Environment/SplitStringIntoPair.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace CodeWars_Environment
+{
+    public static class SplitStringIntoPair
+    {
+        public static void Run()
+        {
+            Console.WriteLine(Solution("abc")); // should return ["ab", "c_"]
+            Console.WriteLine(Solution("abcdef")); // should return ["ab", "cd", "ef"]
+            Console.ReadLine();
+        }
+
+        public static string[] Solution(string str)
+        {
+            // create an array
+            string[] sol = new string[str.Length / 2];
+            // loop string and add 2 characters to the array
+            // if last string < 2 add "_" 
+            return sol;
+        }
+    }
+}
+
