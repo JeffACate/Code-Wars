2eaace3519b0f83dd543117a5784158d3880041d
JeffACate <Jeff.A.Cate@gmail.com>
2021-02-10 19:47:59 -0600

    Split Strings complete

--- Code-Wars-Repo/CodeWars_Environment/SplitStringIntoPair.cs
+++ Code-Wars-Repo/CodeWars_Environment/SplitStringIntoPair.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Web.Helpers;
 
 namespace CodeWars_Environment
 {
@@ -6,18 +7,34 @@ namespace CodeWars_Environment
     {
         public static void Run()
         {
-            Console.WriteLine(Solution("abc")); // should return ["ab", "c_"]
-            Console.WriteLine(Solution("abcdef")); // should return ["ab", "cd", "ef"]
+            Console.WriteLine("abc " + " should return [\"ab\", \"c_\"] =>" + Json.Encode(Solution("abc"))); // should return ["ab", "c_"]
+            Console.WriteLine("abcdef " + " should return [\"ab\", \"cd\", \"ef\"] =>" + Json.Encode(Solution("abcdef"))); // should return ["ab", "cd", "ef"]
             Console.ReadLine();
         }
 
         public static string[] Solution(string str)
         {
-            // create an array
-            string[] sol = new string[str.Length / 2];
-            // loop string and add 2 characters to the array
-            // if last string < 2 add "_" 
-            sol = str.Split(str.Length / 2);
+            string[] sol = new string[Convert.ToInt32(Math.Ceiling((double)str.Length / 2))];
+            int next = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                string pair = "";
+                int j = i + 1;
+                pair = str[i].ToString();
+                if( j <= str.Length-1)
+                {
+                    pair += str[j].ToString();
+                }
+                else
+                {
+                    pair += "_";
+                }
+                if(i%2==0)
+                {
+                    sol[next] = pair;
+                    next++;
+                }
+            }
             return sol;
         }
     }
