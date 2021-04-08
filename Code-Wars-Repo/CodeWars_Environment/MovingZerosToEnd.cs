de206f99eb7227d9856857c530e7aaa78299d4af
JeffACate <Jeff.A.Cate@gmail.com>
2021-04-08 10:30:42 -0500

    Kata complete

--- Code-Wars-Repo/CodeWars_Environment/MovingZerosToEnd.cs
+++ Code-Wars-Repo/CodeWars_Environment/MovingZerosToEnd.cs
@@ -12,20 +12,41 @@ namespace CodeWars_Environment
         {
             int[] sortedArr = MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1});
             DisplayArr(sortedArr);
-            Console.ReadKey();
-
+            
+            sortedArr = MoveZeroes(new int[] { 0, 0, 0});
+            DisplayArr(sortedArr);
         }
 
         static int[] MoveZeroes(int[] arr)
         {
-
+            int count = 0;
+            int j = 0;
+            
+            for (int i = 0; i <= arr.Length - 1; i++)
+            {
+                if(arr[i] == 0)
+                {
+                    count++;
+                }
+                else
+                {
+                    arr[j] = arr[i];
+                    j++;
+                }
+            }
+            while(count > 0)
+            {
+                arr[j] = 0;
+                j++;
+                count--;
+            }
             // TODO: Program me
             return arr;        
         }
 
         static void DisplayArr(int[] arr)
         {
-            Console.Write($"sortedArr: {string.Join(", ", arr)}.");
+            Console.WriteLine($"sortedArr: {string.Join(", ", arr)}");
         }
     }
 }
