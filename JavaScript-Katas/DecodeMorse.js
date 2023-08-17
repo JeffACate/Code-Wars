d88f2c37e170a83ef9841c659459ed5192fdb6dd
JeffACate <Jeff.A.Cate@gmail.com>
2023-08-17 16:21:25 -0500

    Converted to use test 'framework'.

--- JavaScript-Katas/DecodeMorse.js
+++ JavaScript-Katas/DecodeMorse.js
@@ -1,89 +1,111 @@
-function decode(str) {
-    // implement morse decode here
-    if(str === '')
-      return '';
-    arr = str.split(' ')
-    result =  arr.map(function(element) {
-      switch (element) {
-        case ".-":
-          return  "a";
-        case "-...":
-          return  "b";
-        case "-.-.":
-          return  "c";
-        case "-..":
-          return  "d";
-        case ".":
-          return  "e";
-        case "..-.":
-          return  "f";
-        case "--.":
-          return  "g";
-        case "....":
-          return  "h";
-        case "..":
-          return  "i";
-        case ".---":
-          return  "j";
-        case "-.-":
-          return  "k";
-        case ".-..":
-          return  "l";
-        case "--":
-          return  "m";
-        case "-.":
-          return  "n";
-        case "---":
-          return  "o";
-        case ".--.":
-          return  "p";
-        case "--.-":
-          return  "q";
-        case ".-.":
-          return  "r";
-        case "...":
-          return  "s";
-        case "-":
-          return  "t";
-        case "..-":
-          return  "u";
-        case "...-":
-          return  "v";
-        case ".--":
-          return  "w";
-        case "-..-":
-          return  "x";
-        case "-.--":
-          return  "y";
-        case "--..":
-          return  "z";
-        case ".----":
-          return  "1";
-        case "..---":
-          return  "2";
-        case "...--":
-          return  "3";
-        case "....-":
-          return  "4";
-        case ".....":
-          return  "5";
-        case "-....":
-          return  "6";
-        case "--...":
-          return  "7";
-        case "---..":
-          return  "8";
-        case "----.":
-          return  "9";
-        case "-----":
-          return  "0";
-        default:
-          return  " ";
-      }
-    });
-    return result.join("");
-  }
+'using strict';
 
-console.log("\'.... . .-.. .-.. ---  .-- --- .-. .-.. -..  \' → \"hello world\"", decode(".... . .-.. .-.. ---  .-- --- .-. .-.. -..") === "hello world");
-console.log("\'.---- ... -  .- -. -..  ..--- -. -..        \' → \"1st and 2nd\"", decode(".---- ... -  .- -. -..  ..--- -. -..") === "1st and 2nd");
-console.log("                                            \'\' → \'\'", decode("") === "");
+const TestRunner = require('./TestRunner');
+
+function decode(str) {
+  // implement morse decode here
+  if (str === '') return '';
+  arr = str.split(' ');
+  result = arr.map(function (element) {
+    switch (element) {
+      case '.-':
+        return 'a';
+      case '-...':
+        return 'b';
+      case '-.-.':
+        return 'c';
+      case '-..':
+        return 'd';
+      case '.':
+        return 'e';
+      case '..-.':
+        return 'f';
+      case '--.':
+        return 'g';
+      case '....':
+        return 'h';
+      case '..':
+        return 'i';
+      case '.---':
+        return 'j';
+      case '-.-':
+        return 'k';
+      case '.-..':
+        return 'l';
+      case '--':
+        return 'm';
+      case '-.':
+        return 'n';
+      case '---':
+        return 'o';
+      case '.--.':
+        return 'p';
+      case '--.-':
+        return 'q';
+      case '.-.':
+        return 'r';
+      case '...':
+        return 's';
+      case '-':
+        return 't';
+      case '..-':
+        return 'u';
+      case '...-':
+        return 'v';
+      case '.--':
+        return 'w';
+      case '-..-':
+        return 'x';
+      case '-.--':
+        return 'y';
+      case '--..':
+        return 'z';
+      case '.----':
+        return '1';
+      case '..---':
+        return '2';
+      case '...--':
+        return '3';
+      case '....-':
+        return '4';
+      case '.....':
+        return '5';
+      case '-....':
+        return '6';
+      case '--...':
+        return '7';
+      case '---..':
+        return '8';
+      case '----.':
+        return '9';
+      case '-----':
+        return '0';
+      default:
+        return ' ';
+    }
+  });
+  return result.join('');
+}
+
+// console.log("\'.... . .-.. .-.. ---  .-- --- .-. .-.. -..  \' → \"hello world\"", decode(".... . .-.. .-.. ---  .-- --- .-. .-.. -..") === "hello world");
+// console.log("\'.---- ... -  .- -. -..  ..--- -. -..        \' → \"1st and 2nd\"", decode(".---- ... -  .- -. -..  ..--- -. -..") === "1st and 2nd");
+// console.log("                                            \'\' → \'\'", decode("") === "");
+
+const runner = new TestRunner.TestRunner();
+runner.runTests(
+  [
+    {
+      actual: '.... . .-.. .-.. ---  .-- --- .-. .-.. -..',
+      expected: 'hello world',
+    },
+    {
+      actual: '.---- ... -  .- -. -..  ..--- -. -..',
+      expected: '(111) 111-1111',
+    },
+    {
+      actual: '',
+      expected: '',
+    },
+  ],
+  decode
+);
